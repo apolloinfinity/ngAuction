0e54f9e1fcbb60113eded5c40d1fbcc1a0498a7b
Javier Ramirez Roa <apollo.infinity88@gmail.com>
2020-05-19 22:52:35 -0700

    Saving project in it's state

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProductItemComponent } from './components/product-item/product-item.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { SearchComponent } from './components/search/search.component';
+import { StarsComponent } from './components/stars/stars.component';
+import { ProductService } from './shared/product.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HomeComponent,
+		CarouselComponent,
+		FooterComponent,
+		NavbarComponent,
+		ProductItemComponent,
+		ProductDetailComponent,
+		SearchComponent,
+		StarsComponent
+	],
+	imports: [ BrowserModule, AppRoutingModule ],
+	providers: [ ProductService ],
+	bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
